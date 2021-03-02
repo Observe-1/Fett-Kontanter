d0de87a6b308684ec9def02419e2d885d64e7e3c
Observe-1 <louis1407@outlook.com>
2021-03-02 23:22:24 +0000

    Fixing naming mistake

--- screens/FlowsScreen.js
+++ screens/FlowsScreen.js
@@ -1,12 +1,82 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { Button } from 'react-native-material-ui';
+import { Avatar, Card, DataTable, Paragraph } from 'react-native-paper';
+
+function createData(name, amount, percentage) {
+  return { name, amount, percentage};
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0),
+  createData('Ice cream sandwich', 237, 9.0)
+];
 
 export default function FlowsScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Flows!</Text>
-        <Button raised accent text="Button to nowhere lol" />
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        {/* <Button raised accent text="Edit" /> */}
+
+
+        <Card>
+          <Card.Title title="Inflow 💰" subtitle="All money coming in." />
+          <Card.Content>
+            <DataTable >
+                    {/* <DataTable.Header>
+                        <DataTable.Title>Name </DataTable.Title>
+                        <DataTable.Title align="right"> Amount (pcm)</DataTable.Title>
+                        <DataTable.Title align="right">Percentage</DataTable.Title>                     
+                      </DataTable.Header> */}
+                    <DataTable.Row>
+                      <DataTable.Cell>Job Salary 💼</DataTable.Cell>
+                      <DataTable.Cell numeric>£2,500.00</DataTable.Cell>
+                      <DataTable.Cell numeric>83.3%</DataTable.Cell>
+                    </DataTable.Row>
+
+                    <DataTable.Row>
+                      <DataTable.Cell>Bonus 🏆</DataTable.Cell>
+                      <DataTable.Cell numeric>£500.00</DataTable.Cell>
+                      <DataTable.Cell numeric>16.7%</DataTable.Cell>
+                    </DataTable.Row>
+                </DataTable>
+          </Card.Content>          
+        </Card>
+
+
+        {/* TODO Remove below*/}
+        <Text> </Text>
+        <Card>
+          <Card.Title title="Outflow 💸" subtitle="Essential money going out." />
+          <Card.Content>
+            <DataTable >
+                    {/* <DataTable.Header>
+                        <DataTable.Title>Name </DataTable.Title>
+                        <DataTable.Title align="right"> Amount (pcm)</DataTable.Title>
+                        <DataTable.Title align="right">Percentage</DataTable.Title>                     
+                      </DataTable.Header> */}
+                    <DataTable.Row>
+                      <DataTable.Cell>Rent 🏡</DataTable.Cell>
+                      <DataTable.Cell numeric>£1,000.00</DataTable.Cell>
+                      <DataTable.Cell numeric>58.8%</DataTable.Cell>
+                    </DataTable.Row>
+
+                    <DataTable.Row>
+                      <DataTable.Cell>Food 🍕</DataTable.Cell>
+                      <DataTable.Cell numeric>£500.00</DataTable.Cell>
+                      <DataTable.Cell numeric>29.4%</DataTable.Cell>
+                    </DataTable.Row>
+
+                    <DataTable.Row>
+                      <DataTable.Cell>Utilities⚡</DataTable.Cell>
+                      <DataTable.Cell numeric>£200.00</DataTable.Cell>
+                      <DataTable.Cell numeric>11.8%</DataTable.Cell>
+                    </DataTable.Row>
+                </DataTable>
+                {/* TODO Remove below*/}
+                <Text> </Text>
+                <Paragraph>❌ Estimated other costs £1300 ❌</Paragraph>
+          </Card.Content>          
+        </Card>
       </View>
     );
   }
