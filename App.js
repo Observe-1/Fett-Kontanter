e57b02e766d63fa6c49c658a2af807ba09207f3a
Observe-1 <louis1407@outlook.com>
2021-03-02 18:14:27 +0000

    First implementation

--- App.js
+++ App.js
@@ -1,25 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar'
 import { Button } from 'react-native-material-ui';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>      
-      <StatusBar style="auto" />
-      <View>
-          <Text>Welcome to Financial...</Text>
-          <Button raised accent text="Button to nowhere lol" />
-      </View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+      <Button raised text="Button to nowhere lol" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+      <Button raised accent text="Button to nowhere lol" />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
