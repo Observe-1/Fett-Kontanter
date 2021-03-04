f503317b0e1b85e191dcfe96864f5b4f602e8bc2
Observe-1 <louis1407@outlook.com>
2021-03-04 09:53:32 +0000

    First working labels

--- screens/PortfolioScreen.js
+++ screens/PortfolioScreen.js
@@ -1,79 +1,58 @@
-import * as React from "react";
-import { Text, View } from "react-native";
-import { Button } from "react-native-material-ui";
-import { Grid } from "react-native-svg-charts";
-import * as shape from "d3-shape";
-import { AreaChart, PieChart, LineChart } from "react-native-svg-charts";
+import React from "react";
+import { PieChart } from "react-native-svg-charts";
+import { View } from "react-native";
+import { Circle, G, Line, Text } from "react-native-svg";
 
-export default function PortfolioScreen() {
-    const data = [
-        {
-            key: 1,
-            value: 50,
-            svg: { fill: "#600080" },
-            arc: { outerRadius: "130%", cornerRadius: 10 },
-        },
-        {
-            key: 2,
-            value: 50,
-            svg: { fill: "#9900cc" },
-        },
-        {
-            key: 3,
-            value: 40,
-            svg: { fill: "#c61aff" },
-        },
-        {
-            key: 4,
-            value: 95,
-            svg: { fill: "#d966ff" },
-        },
-        {
-            key: 5,
-            value: 35,
-            svg: { fill: "#ecb3ff" },
-        },
-    ];
+function PieChartWithLabelExample() {
+    const data = [50, 40, 95, -4, -24, 85, 91];
 
-    const Labels = ({ slices, height, width }) => {
+    const randomColor = () =>
+        ("#" + ((Math.random() * 0xffffff) << 0).toString(16) + "000000").slice(
+            0,
+            7,
+        );
+
+    const pieData = data
+        .filter((value) => value > 0)
+        .map((value, index) => ({
+            value,
+            svg: { fill: randomColor() },
+            key: `pie-${index}`,
+        }));
+
+    const Labels = ({ slices }) => {
         return slices.map((slice, index) => {
             const { labelCentroid, pieCentroid, data } = slice;
             return (
-                <Text
-                    key={index}
-                    x={pieCentroid[0]}
-                    y={pieCentroid[1]}
-                    fill={"white"}
-                    textAnchor={"middle"}
-                    alignmentBaseline={"middle"}
-                    fontSize={24}
-                    stroke={"black"}
-                    strokeWidth={0.2}
-                    style={{
-                        // position: "absolute",
-                        textAlign: "center",
-                    }}
-                >
-                    {pieCentroid[0]}
-                </Text>
+                <G key={index}>
+                    <Text
+                        key={index}
+                        x={pieCentroid[0]}
+                        y={pieCentroid[1]}
+                        fill={"black"}
+                        textAnchor={"middle"}
+                        alignmentBaseline={"middle"}
+                        stroke={"black"}
+                        strokeWidth={0.2}
+                    >
+                        {data.value}
+                    </Text>
+                </G>
             );
         });
     };
 
     return (
-        <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        <PieChart
+            style={{ height: 200 }}
+            data={pieData}
+            innerRadius={20}
+            outerRadius={55}
+            labelRadius={80}
         >
-            <PieChart
-                style={{ width: 200, height: 200 }}
-                valueAccessor={({ item }) => item.value}
-                outerRadius={"70%"}
-                innerRadius={10}
-                data={data}
-                spacing={0}
-            >
-                <Labels />
-            </PieChart>
-        </View>
+            <Labels />
+        </PieChart>
     );
 }
+
+export default PieChartWithLabelExample;
