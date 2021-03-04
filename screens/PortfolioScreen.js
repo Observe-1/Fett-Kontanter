416e7c36cc32f492caffec93a04b647b4805893c
Observe-1 <louis1407@outlook.com>
2021-03-04 11:06:42 +0000

    Secondry chart with spacing

--- screens/PortfolioScreen.js
+++ screens/PortfolioScreen.js
@@ -43,6 +43,34 @@ export default function PortfolioScreen() {
         },
     ];
 
+    const data2 = [
+        {
+            key: 1,
+            value: 80,
+            pounds: "£80k",
+            svg: { fill: "#d02860" },
+        },
+        {
+            key: 2,
+            value: 50,
+            pounds: "£50k",
+            svg: { fill: "#FF6500" },
+        },
+        {
+            key: 3,
+            value: 100,
+            pounds: "£100k",
+            svg: { fill: "#d02800" },
+        },
+        {
+            key: 4,
+            value: 35,
+            pounds: "£35k",
+            svg: { fill: "#FF0000" },
+            arc: { outerRadius: "130%", cornerRadius: 10 },
+        },
+    ];
+
     //TODO Make default theme
     const theme = {
         roundness: 30,
@@ -50,7 +78,13 @@ export default function PortfolioScreen() {
 
     return (
         <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+            style={{
+                flex: 1,
+                justifyContent: "space-between",
+                alignItems: "center",
+                maxHeight: 450,
+                top: "10%",
+            }}
         >
             <Card
                 theme={theme}
@@ -65,6 +99,21 @@ export default function PortfolioScreen() {
                     }}
                 >
                     <LabelledPieChart pieData={data} />
+                </Card.Content>
+            </Card>
+            <Card
+                theme={theme}
+                elevation={5}
+                style={{ height: 200, width: 200 }}
+            >
+                <Card.Content
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <LabelledPieChart pieData={data2} />
                 </Card.Content>
             </Card>
         </View>
