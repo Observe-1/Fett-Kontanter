3c822d1ae3c793a9e7de1924fb409d08aabb1112
Observe-1 <louis1407@outlook.com>
2021-03-05 16:09:44 +0000

    Working grid

--- screens/PortfolioScreen.js
+++ screens/PortfolioScreen.js
@@ -13,77 +13,138 @@ export default function PortfolioScreen() {
     const data = [
         {
             key: 1,
-            value: 50,
-            pounds: "£50k",
-            svg: { fill: "#FF6500" },
-            arc: { outerRadius: "130%", cornerRadius: 10 },
+            value: [
+                {
+                    key: 1,
+                    value: 50,
+                    pounds: "£50k",
+                    svg: { fill: "#FF6500" },
+                    arc: { outerRadius: "130%", cornerRadius: 10 },
+                },
+                {
+                    key: 2,
+                    value: 50,
+                    pounds: "£50k",
+                    svg: { fill: "#d02860" },
+                },
+                {
+                    key: 3,
+                    value: 40,
+                    pounds: "£40k",
+                    svg: { fill: "#d02800" },
+                },
+                {
+                    key: 4,
+                    value: 95,
+                    pounds: "£95k",
+                    svg: { fill: "#d00060" },
+                },
+                {
+                    key: 5,
+                    value: 35,
+                    pounds: "£35k",
+                    svg: { fill: "#FF0000" },
+                },
+            ],
         },
         {
             key: 2,
-            value: 50,
-            pounds: "£50k",
-            svg: { fill: "#d02860" },
+            value: [
+                {
+                    key: 1,
+                    value: 80,
+                    pounds: "£80k",
+                    svg: { fill: "#d02860" },
+                },
+                {
+                    key: 2,
+                    value: 50,
+                    pounds: "£50k",
+                    svg: { fill: "#FF6500" },
+                },
+                {
+                    key: 3,
+                    value: 100,
+                    pounds: "£100k",
+                    svg: { fill: "#d02800" },
+                },
+                {
+                    key: 4,
+                    value: 35,
+                    pounds: "£35k",
+                    svg: { fill: "#FF0000" },
+                },
+            ],
         },
         {
             key: 3,
-            value: 40,
-            pounds: "£40k",
-            svg: { fill: "#d02800" },
+            value: [
+                {
+                    key: 1,
+                    value: 80,
+                    pounds: "£80k",
+                    svg: { fill: "#d02860" },
+                },
+                {
+                    key: 2,
+                    value: 50,
+                    pounds: "£50k",
+                    svg: { fill: "#FF6500" },
+                },
+            ],
         },
         {
             key: 4,
-            value: 95,
-            pounds: "£95k",
-            svg: { fill: "#d00060" },
-        },
-        {
-            key: 5,
-            value: 35,
-            pounds: "£35k",
-            svg: { fill: "#FF0000" },
-        },
-    ];
-
-    const data2 = [
-        {
-            key: 1,
-            value: 80,
-            pounds: "£80k",
-            svg: { fill: "#d02860" },
-        },
-        {
-            key: 2,
-            value: 50,
-            pounds: "£50k",
-            svg: { fill: "#FF6500" },
-        },
-        {
-            key: 3,
-            value: 100,
-            pounds: "£100k",
-            svg: { fill: "#d02800" },
-        },
-        {
-            key: 4,
-            value: 35,
-            pounds: "£35k",
-            svg: { fill: "#FF0000" },
-            arc: { outerRadius: "130%", cornerRadius: 10 },
-        },
-    ];
-
-    const data3 = [
-        {
-            key: 1,
-            value: 80,
-            pounds: "£80k",
-            svg: { fill: "#d02860" },
-        },
-        {
-            key: 2,
-            value: 50,
-            pounds: "£50k",
-            svg: { fill: "#FF6500" },
+            value: [
+                {
+                    key: 1,
+                    value: 80,
+                    pounds: "£80k",
+                    svg: { fill: "#d02860" },
+                },
+                {
+                    key: 2,
+                    value: 50,
+                    pounds: "£50k",
+                    svg: { fill: "#FF6500" },
+                },
+                {
+                    key: 3,
+                    value: 80,
+                    pounds: "£80k",
+                    svg: { fill: "#d02860" },
+                },
+                {
+                    key: 4,
+                    value: 50,
+                    pounds: "£50k",
+                    svg: { fill: "#FF6500" },
+                },
+                {
+                    key: 5,
+                    value: 80,
+                    pounds: "£80k",
+                    svg: { fill: "#d02860" },
+                },
+                {
+                    key: 6,
+                    value: 50,
+                    pounds: "£50k",
+                    svg: { fill: "#FF6500" },
+                },
+                {
+                    key: 7,
+                    value: 80,
+                    pounds: "£80k",
+                    svg: { fill: "#d02860" },
+                },
+                {
+                    key: 8,
+                    value: 50,
+                    pounds: "£50k",
+                    svg: { fill: "#FF6500" },
+                },
+            ],
         },
     ];
 
@@ -110,15 +171,18 @@ export default function PortfolioScreen() {
                 }}
                 columnWrapperStyle={{ justifyContent: "space-between" }}
                 numColumns={2}
-                data={data2}
+                data={data}
                 keyExtractor={(item, index) => index.toString()}
                 renderItem={({ item }) => {
-                    console.log(item);
                     return (
                         <Card
                             theme={theme}
                             elevation={5}
-                            style={{ height: "75%", width: "45%" }}
+                            style={{
+                                height: "75%",
+                                width: "45%",
+                                minWidth: 150,
+                            }}
                         >
                             <Card.Content
                                 style={{
@@ -128,43 +192,12 @@ export default function PortfolioScreen() {
                                     top: "-20%",
                                 }}
                             >
-                                <LabelledPieChart pieData={data} />
+                                <LabelledPieChart pieData={item.value} />
                             </Card.Content>
                         </Card>
                     );
                 }}
             />
-            <Text>Ingredients</Text>
-            {/* <Card
-                theme={theme}
-                elevation={5}
-                style={{ height: 200, width: 200 }}
-            >
-                <Card.Content
-                    style={{
-                        flex: 1,
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <LabelledPieChart pieData={data} />
-                </Card.Content>
-            </Card>
-            <Card
-                theme={theme}
-                elevation={5}
-                style={{ height: 200, width: 200 }}
-            >
-                <Card.Content
-                    style={{
-                        flex: 1,
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <LabelledPieChart pieData={data2} />
-                </Card.Content>
-            </Card> */}
         </View>
     );
 }
