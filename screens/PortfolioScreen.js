3a22115db3bb448440313c756c29e039f24f89b6
adil <adilkhwaja7@gmail.com>
2021-03-05 17:10:08 +0000

    bare stuff going on

--- screens/PortfolioScreen.js
+++ screens/PortfolioScreen.js
@@ -1,101 +1,27 @@
 import React from "react";
-import { LineChart, Grid } from "react-native-svg-charts";
-import * as shape from "d3-shape";
-import { Circle, G, Line, Rect, Text } from "react-native-svg";
+import { StyleSheet, View } from "react-native";
 
-export default function PortfolioScreen() {
-    const data = [
-        50,
-        10,
-        40,
-        95,
-        -4,
-        -24,
-        85,
-        91,
-        35,
-        53,
-        -53,
-        24,
-        50,
-        -20,
-        -80,
-    ];
+import Model from "../Components/Cursor";
 
-    /**
-     * Both below functions should preferably be their own React Components
-     */
+const data = [
+  { date: new Date(2018, 9, 1).getTime(), value: 0 },
+  { date: new Date(2018, 9, 16).getTime(), value: 0 },
+  { date: new Date(2018, 9, 17).getTime(), value: 200 },
+  { date: new Date(2018, 10, 1).getTime(), value: 200 },
+  { date: new Date(2018, 10, 2).getTime(), value: 300 },
+  { date: new Date(2018, 10, 5).getTime(), value: 300 }
+];
 
-    const HorizontalLine = ({ y }) => (
-        <Line
-            key={"zero-axis"}
-            x1={"0%"}
-            x2={"100%"}
-            y1={y(50)}
-            y2={y(50)}
-            stroke={"grey"}
-            strokeDasharray={[4, 8]}
-            strokeWidth={2}
-        />
-    );
+export default () => (
+  <View style={styles.container}>
+    <Model {...{ data }} />
+  </View>
+);
 
-    const Tooltip = ({ x, y }) => (
-        <G
-            x={x(5) - 75 / 2}
-            key={"tooltip"}
-            onPress={() => console.log("tooltip clicked")}
-        >
-            <G y={50}>
-                <Rect
-                    height={40}
-                    width={75}
-                    stroke={"grey"}
-                    fill={"white"}
-                    ry={10}
-                    rx={10}
-                />
-                <Text
-                    x={75 / 2}
-                    dy={20}
-                    alignmentBaseline={"middle"}
-                    textAnchor={"middle"}
-                    stroke={"rgb(134, 65, 244)"}
-                >
-                    {`${data[5]}ºC`}
-                </Text>
-            </G>
-            <G x={75 / 2}>
-                <Line
-                    y1={50 + 40}
-                    y2={y(data[5])}
-                    stroke={"grey"}
-                    strokeWidth={2}
-                />
-                <Circle
-                    cy={y(data[5])}
-                    r={6}
-                    stroke={"rgb(134, 65, 244)"}
-                    strokeWidth={2}
-                    fill={"white"}
-                />
-            </G>
-        </G>
-    );
-
-    return (
-        <LineChart
-            style={{ height: 200 }}
-            data={data}
-            svg={{
-                stroke: "rgb(134, 65, 244)",
-                strokeWidth: 2,
-            }}
-            contentInset={{ top: 20, bottom: 20 }}
-            curve={shape.curveLinear}
-        >
-            <Grid />
-            <HorizontalLine />
-            <Tooltip />
-        </LineChart>
-    );
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
