693a43a4852078c7b68d43253cf78ffeaf8597b3
Observe-1 <louis1407@outlook.com>
2021-03-28 14:33:57 +0100

    Improved First Launch Scene

--- screens/FirstLaunchScreen.js
+++ screens/FirstLaunchScreen.js
@@ -7,15 +7,37 @@ export default function FirstLaunchScreen() {
     const [text, onChangeText] = React.useState("");
 
     return (
-        <View style={{ flex: 1, justifyContent: "center" }}>
-            <Card>
-                <Card.Title
-                    title="Hello!"
-                    subtitle="Welcome to Fett Kontanter. What's your name?"
-                />
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: "#253041",
+                justifyContent: "center",
+            }}
+        >
+            <Card
+                style={{
+                    alignSelf: "center",
+                    width: "85%",
+                    padding: 15,
+                }}
+            >
+                <Text style={{ fontSize: 40, margin: 20 }}>Hello!</Text>
+                <Text style={{ fontSize: 15, margin: 20 }}>
+                    Welcome to Fett Kontanter. What's your name?
+                </Text>
                 <Card.Content>
-                    <TextInput onChangeText={onChangeText} value={text} />
-                    <Button raised primary text="Submit" />
+                    <TextInput
+                        style={{ margin: 20 }}
+                        onChangeText={onChangeText}
+                        placeholder="Your Name"
+                        value={text}
+                    />
+                    <Button
+                        styles={{ margin: 15 }}
+                        raised
+                        primary
+                        text="Submit"
+                    />
                 </Card.Content>
             </Card>
         </View>
