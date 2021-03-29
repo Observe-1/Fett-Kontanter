547547c9f234cd4aff3a2f3981c3afede4b3fc3d
Observe-1 <louis1407@outlook.com>
2021-03-29 15:29:42 +0100

    Conditional rendering example

--- screens/OverviewScreen.js
+++ screens/OverviewScreen.js
@@ -9,26 +9,25 @@ export default function OverviewScreen(props) {
     const [user, onChangeUser] = React.useState("");
 
     const [items, setItems] = React.useState([
-        { name: "TURQUOISE", code: "#1abc9c" },
-        { name: "EMERALD", code: "#2ecc71" },
-        { name: "PETER RIVER", code: "#3498db" },
-        { name: "AMETHYST", code: "#9b59b6" },
-        { name: "WET ASPHALT", code: "#34495e" },
-        { name: "GREEN SEA", code: "#16a085" },
-        { name: "NEPHRITIS", code: "#27ae60" },
-        { name: "BELIZE HOLE", code: "#2980b9" },
-        { name: "WISTERIA", code: "#8e44ad" },
-        { name: "MIDNIGHT BLUE", code: "#2c3e50" },
-        { name: "SUN FLOWER", code: "#f1c40f" },
-        { name: "CARROT", code: "#e67e22" },
-        { name: "ALIZARIN", code: "#e74c3c" },
-        { name: "CLOUDS", code: "#ecf0f1" },
-        { name: "CONCRETE", code: "#95a5a6" },
-        { name: "ORANGE", code: "#f39c12" },
-        { name: "PUMPKIN", code: "#d35400" },
-        { name: "POMEGRANATE", code: "#c0392b" },
-        { name: "SILVER", code: "#bdc3c7" },
-        { name: "ASBESTOS", code: "#7f8c8d" },
+        { name: "TURQUOISE", code: "#1abc9c", category: "pieChart" },
+        { name: "EMERALD", code: "#2ecc71", category: "lineChart" },
+        { name: "PETER RIVER", code: "#3498db", category: "text" },
+        { name: "AMETHYST", code: "#9b59b6", category: "text" },
+        { name: "WET ASPHALT", code: "#34495e", category: "table" },
+        { name: "GREEN SEA", code: "#16a085", category: "lineChart" },
+        { name: "NEPHRITIS", code: "#27ae60", category: "table" },
+        { name: "BELIZE HOLE", code: "#2980b9", category: "text" },
+        { name: "WISTERIA", code: "#8e44ad", category: "text" },
+        { name: "MIDNIGHT BLUE", code: "#2c3e50", category: "text" },
+        { name: "SUN FLOWER", code: "#f1c40f", category: "text" },
+        { name: "CARROT", code: "#e67e22", category: "table" },
+        { name: "ALIZARIN", code: "#e74c3c", category: "lineChart" },
+        { name: "CONCRETE", code: "#95a5a6", category: "text" },
+        { name: "ORANGE", code: "#f39c12", category: "text" },
+        { name: "PUMPKIN", code: "#d35400", category: "text" },
+        { name: "POMEGRANATE", code: "#c0392b", category: "lineChart" },
+        { name: "SILVER", code: "#bdc3c7", category: "text" },
+        { name: "ASBESTOS", code: "#7f8c8d", category: "text" },
     ]);
 
     if (user == "") {
@@ -63,8 +62,27 @@ export default function OverviewScreen(props) {
                             { backgroundColor: item.code },
                         ]}
                     >
-                        <Text style={styles.itemName}>{item.name}</Text>
-                        <Text style={styles.itemCode}>{item.code}</Text>
+                        {item.category == "text" && (
+                            <View>
+                                <Text style={styles.itemName}>{item.name}</Text>
+                                <Text style={styles.itemCode}>{item.code}</Text>
+                            </View>
+                        )}
+                        {item.category == "pieChart" && (
+                            <Text style={styles.itemName}>
+                                A pie chart goes here
+                            </Text>
+                        )}
+                        {item.category == "lineChart" && (
+                            <Text style={styles.itemName}>
+                                A line chart goes here
+                            </Text>
+                        )}
+                        {item.category == "table" && (
+                            <Text style={styles.itemName}>
+                                A table goes here
+                            </Text>
+                        )}
                     </View>
                 )}
             />
